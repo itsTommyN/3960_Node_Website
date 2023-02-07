005485a4554c508c15897931bc0d47addf2aeb86
Tommy Nguyen <tommylulxdd@gmail.com>
2023-02-07 13:07:07 -0800

    Updated code and mongodb is properly connected now

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-// const expireTime = 24 * 60 * 60 * 1000;
+const expireTime = 24 * 60 * 60 * 1000;
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -25,7 +25,7 @@ const node_session_secret = process.env.NODE_SESSION_SECRET;
 /* END secret section */
 
 var mongoStore = MongoStore.create({
-    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}cluster0.dq3nbpy.mongodb.net/sessions`,
+    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@cluster0.dq3nbpy.mongodb.net/sessions`,
     crypto: {
         secret: mongodb_session_secret
     }
@@ -41,10 +41,10 @@ app.use(session({
 // Main Menu
 app.get('/', (req, res) => {
     var html = `
-    <form action='/signup' method='post'>
+    <form action='/signup' method='get'>
         <button>Sign up</button>
     </form>
-    <form action='/login' method='post'>
+    <form action='/login' method='get'>
         <button>Log in</button>
     </form>
     `;
@@ -52,18 +52,25 @@ app.get('/', (req, res) => {
 });
 
 // route to Signup
-app.post('/signup', (req, res) => {
-    var missingEmail = req.query.missing;
+app.get('/signup', (req, res) => {
+    var missingEmail = req.query.emailmia;
+    var missingPass = req.query.passwordmia;
+    var missingUser = req.query.usernamemia;
 
     var html = `
     <form action='/submitUser' method='post'>
-      <input name='email' type='text' placeholder='email'>
+    <input name='username' type='text' placeholder='username'>
+    <input name='email' type='email' placeholder='email'>
+    <input name='password' type='password' placeholder='password'>
       <button>Submit</button>
     </form>
   `;
-
-    if (missingEmail) {
-        html += "email is required"
+    if (missingUser) {
+        html += "<br> Please provide a Username."
+    } else if (missingEmail) {
+        html += "<br> Please provide an Email Address."
+    } else if (missingPass) {
+        html += "<br> Please provide a Password."
     }
 
     res.send(html);
@@ -74,15 +81,15 @@ app.post('/submitUser', (req, res) => {
     var password = req.body.password;
     var email = req.body.email;
     if (!email) {
-        res.redirect('/signup?missing=1');
+        res.redirect('/signup?emailmia=1');
     } else if (!password) {
-        res.redirect(`/signup?error=Invalid Password`);
+        res.redirect(`/signup?passwordmia=1`);
     } else if (!username) {
-        return res.redirect(`/signup?error=Invalid Username`)
+        return res.redirect(`/signup?usernamemia=1`)
     } else {
         var hashedPassword = bcrypt.hashSync(password, saltRounds);
 
-        users.push({ username: username, password: hashedPassword });
+        users.push({ username: username, email: email, password: hashedPassword });
 
 
         res.redirect('/login')
@@ -90,49 +97,74 @@ app.post('/submitUser', (req, res) => {
 });
 
 // route to Login
-app.post('/login', (req, res) => {
-    var username = req.body.username
+app.get('/login', (req, res) => {
+    var missingEmail = req.query.emailmia;
+    var missingPass = req.query.passwordmia;
+
     var html =
         `log in
-    <form action='/members' method='post'>
+    <form action='/loggingin' method='post'>
         <input name='email' type='email' placeholder='email'>
         <input name='password' type='password' placeholder='password'>
         <button>Submit</button>
     </form>
     `
+    if (missingEmail && missingPass) {
+        html += "<br> Email and Password not found."
+    } else if (missingEmail) {
+        html += "<br> Email not found."
+    } else if (missingPass) {
+        html += "<br> Password not found."
+    }
     res.send(html);
 });
 
 app.post('/loggingin', (req, res) => {
-    var username = req.body.username;
+    var email = req.body.email;
     var password = req.body.password;
 
+    if (!email && !password) {
+        res.redirect('/login?emailmia=1&passwordmia=1');
+    } else if (!email) {
+        res.redirect('/login?emailmia=1');
+    } else if (!password) {
+        res.redirect(`/login?passwordmia=1`);
+    } else {
+        for (i = 0; i < users.length; i++) {
+            if (users[i].email == email) {
+                if (bcrypt.compareSync(password, users[i].password)) {
+                    req.session.authenticated = true;
+                    req.session.email = email;
+                    req.session.cookie.maxAge = expireTime;
 
-    var usershtml = "";
-    for (i = 0; i < users.length; i++) {
-        if (users[i].username == username) {
-            if (bcrypt.compareSync(password, users[i].password)) {
-                req.session.authenticated = true;
-                req.session.username = username;
-                req.session.cookie.maxAge = expireTime;
-
-                res.redirect('/members');
-                return;
+                    res.redirect('/members');
+                    return;
+                }
             }
         }
+        //user and password combination not found
+        res.redirect("/login");
     }
-
-    //user and password combination not found
-    res.redirect("/login");
 });
 
 app.get('/members', (req, res) => {
     if (!req.session.authenticated) {
+        console.log("Hello")
         res.redirect('/login');
     }
+    var images = [
+        'goodone.jpg',
+        'ohho.jpg',
+        'thegoodhandshake.jpg'
+    ]
+
+    const randomIndex = Math.floor(Math.random() * images.length);
     var html = `
-    <h1>Hi</hi>
-    `;
+    <h1>Hello ` + req.session.email + `</hi>
+    ` + `<br><img src="` + images[randomIndex] + `"/>
+    <form action='/' method='get'>
+        <button>Sign out</button>
+    </form>`;
     res.send(html);
 });
 
